ae6a9c15f93373d4c7b3a8a2a54907e4c4695e6f
mpaulaortiz <paulaortiz.97@hotmail.com>
2021-03-12 11:48:46 -0500

    Se añadio  funciones para loguearse

--- js/login.js
+++ js/login.js
@@ -3,6 +3,14 @@ let url = 'http://127.0.0.1:1337';
 function login() {
   let router = '/auth/local';
   let token = '';
+  let email =''; 
+  let password= '';
+
+  email= document.getElementById ('email').value
+  password = document.getElementById('password').value
+
+  console.log(email)
+  console.log(password) 
 
   fetch(url + router, {
     method: 'POST',
